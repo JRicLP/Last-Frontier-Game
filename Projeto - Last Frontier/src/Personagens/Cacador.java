68c17f559973407039fa98525d3d447b6a32f8ea
hann1105 <paolahanna068@gmail.com>
2025-03-22 12:03:39 -0300

    Update Cacador.java

--- Projeto - Last Frontier/src/Personagens/Cacador.java
+++ Projeto - Last Frontier/src/Personagens/Cacador.java
@@ -23,4 +23,43 @@ public class Cacador {
         this.habilidadesCacador="Lutador";
         this.localizacaoCacador=localizacaoCacador;
     }
+    //Métodos getter para acessar os valores dos atributos
+    public String getNomeCacador() {
+        return nomeCacador;
+    }
+    public int getFomeCacador() {
+        return fomeCacador;
+    }
+    public int getVidaCacador() {
+        return vidaCacador;
+    }
+    public int getSedeCacador() {
+        return sedeCacador;
+    }
+    public int getSanidadeCacador() {
+        return sanidadeCacador;
+    }
+    public int getEnergiaCacador() {
+        return energiaCacador;
+    }
+    public String getInventarioCacador() {
+        return inventarioCacador;
+    }
+    public String getHabilidadesCacador() {
+        return habilidadesCacador;
+    }
+    public String getLocalizacaoCacador() {
+        return localizacaoCacador;
+    }
+    //Método para mostrar status
+    public void mostrarStatus(){
+        System.out.println("Nome: " + nomeCacador);
+        System.out.println("Vida: " + vidaCacador);
+        System.out.println("Sede: " + sedeCacador);
+        System.out.println("Fome: "+ fomeCacador);
+        System.out.println("Energia: "+ energiaCacador);
+        System.out.println("Sanidade: "+ sanidadeCacador);
+        System.out.println(" Inventário: "+ inventarioCacador);
+        System.out.println("Localização: "+ localizacaoCacador);
+    }
 }
