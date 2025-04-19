24e36257e43a66bf3307a18b3637f44a65f24fb4
hann1105 <paolahanna068@gmail.com>
2025-04-19 18:37:52 -0300

    Update Agua.java

--- Projeto - Last Frontier/src/itens/Agua.java
+++ Projeto - Last Frontier/src/itens/Agua.java
@@ -24,7 +24,33 @@ public class Agua extends Item {
         return volumeAgua;
     }
     //Metodo sobrescrito:
-    public void usar(String nomeItem) {
+  public void beber(Personagem personagem, Remedios antidoto) {
+        personagem.setSedePersonagem(0);
+        if(this.getPurezaAgua()>=7){
+            System.out.println("Sede saciada! Continue sua jornada");
+        }else{
+            System.out.println("Sua sede foi restaurada mas era água contaminada!!!");
+            System.out.println("Você precisa de um antídoto!!!");
+            if (!antidoto.getUsado() && antidoto.usar(personagem)){
+                System.out.println("Antídoto usado com sucesso! Vida estabilizada.");
+                System.out.println("Vida remanescente: "+ personagem.getVidaPersonagem());
+            }
+            else{
+                danoPorContaminacao(personagem);
+            }
+        }
+    }
+    private void danoPorContaminacao(Personagem personagem) {
+        while (personagem.getVidaPersonagem() > 0) {
+            System.out.println("Seus pontos de vida estão baixando...: " + personagem.getVidaPersonagem());
+            personagem.setVidaPersonagem(personagem.getVidaPersonagem() - 2);
+
+            if (personagem.getVidaPersonagem() <= 0) {
+                System.out.println("Você morreu por causa da água contaminada!");
+                break;
+            }
+
+        }
 
     }
 }
