1344df19c68645003a3dc9b014d60ae9fb5abb3f
hann1105 <paolahanna068@gmail.com>
2025-05-19 23:19:02 -0300

    Add files via upload

---
+++ Projeto - Last Frontier/src/mensagensDisplay/CondicaoDeVitoriaDerrota.java
@@ -0,0 +1,23 @@
+package mensagensDisplay;
+
+
+public class CondicaoDeVitoriaDerrota {
+    public void condicaoDisplay() {
+        System.out.println(" -------------------------------------------------------------------ATENÇÃO ----------------------------------------------------------------");
+        System.out.println("Preste atenção para as condições de vitória bravo aventureiro");
+        System.out.println(" 1° - Sobrevivência por Tempo Determinado: Se você conseguir sobreviver até o final do último ambiente terá a vitória conquistada");
+        System.out.println(" 2° - Descoberta de um Refúgio Seguro: Se Você encontrar um refúgio seguro para se instalar, sem criaturas ou qualquer perigo, conquistará a vitória");
+        System.out.println(" 3° - Construção de um Abrigo Permanente: Se você conseguir materiais suficientes para construir um abrigo seguro, vencerá!");
+        System.out.println(" 4° - Resgate Bem-Sucedido: Se você conseguir ativar um pedido de resgate, e sobreviver até ser salvo, vencerá!");
+
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("Preste condição para as condições de derrota bravo aventureiro:");
+        System.out.println("1° - Vida Chegando a Zero: Cuidado! Se sua vida chegar a zero, você MORRERÁ, e perderá o jogo");
+        System.out.println("2° - Morte por Fome ou Sede: Cuidado! Se os pontos de fome ou sede chegarem a 0, você MORRERÁ por desnutrição ou desitração, e perderá o jogo");
+        System.out.println("3° - Perda Total de Sanidade: Cuidado! Se a sua sanidade chegar a 0, você FICARÁ LOUCO e MORRERÁ");
+        System.out.println("4° - Esgotamento de Recursos Essenciais: Cuidado! Sem comida, sem água e sem ferramentas a sobrevivência será IMPOSSIVEL, você morrerá e perderá o jogo");
+        System.out.println("5° - Captura ou Morte por Outros Sobreviventes: Cuidado! Há os Skarnvaldr, Guerreiros Corrompidos, eles irão te atacar e querer matar você, se você for derrotado por eles MORRERÁ e perderá o jogo ");
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("                                    Pressione ENTER para continuar...");
+    }
+}
