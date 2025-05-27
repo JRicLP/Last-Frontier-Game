b507fdc0da8cc88a38e7f77308eb351d4cda825e
hann1105 <paolahanna068@gmail.com>
2025-05-26 22:36:27 -0300

    Add files via upload

---
+++ interface/src/src/eventos/EventoCriatura.java
@@ -0,0 +1,52 @@
+package eventos;
+
+public class EventoCriatura extends Eventos{
+
+    //Atributos da subclasse:
+    private String tipoCriatura;
+    private int vidaCriatura;//Novo atributo adicionado para utilizar o metodo atacar() em Armas
+    private int distanciaCriatura;//Novo atributo adicionado para utilizar o metodo atacar() em Armas, deverá ser revisto
+    private int nivelPerigo;
+    private String opcaoAcao; //Revir a necessidade desse atributo
+
+    //Métodos da subclasse:
+    public EventoCriatura(String nomeEvento, String descricaoEvento, int probabilidadeEvento, String impactoEvento, boolean condicaoEvento,
+                          String tipoCriatura, int vidaCriatura, int distanciaCriatura, int nivelPerigo, String opcaoAcao) {
+        super(nomeEvento, descricaoEvento, probabilidadeEvento, impactoEvento, condicaoEvento);
+        this.tipoCriatura = tipoCriatura;
+        this.vidaCriatura = vidaCriatura;
+        this.distanciaCriatura = distanciaCriatura;
+        this.nivelPerigo = nivelPerigo;
+        this.opcaoAcao = opcaoAcao;
+    }
+    public void setTipoCriatura(String tipoCriatura){
+        this.tipoCriatura = tipoCriatura;
+    }
+    public String getTipoCriatura(){
+        return tipoCriatura;
+    }
+    public void setVidaCriatura(int vidaCriatura) {
+        this.vidaCriatura = vidaCriatura;
+    }
+    public int getVidaCriatura() {
+        return vidaCriatura;
+    }
+    public void setDistanciaCriatura(int distanciaCriatura) {
+        this.distanciaCriatura = distanciaCriatura;
+    }
+    public int getDistanciaCriatura() {
+        return distanciaCriatura;
+    }
+    public void setNivelPerigo(int nivelPerigo){
+        this.nivelPerigo = nivelPerigo;
+    }
+    public int getNivelPerigo(){
+        return nivelPerigo;
+    }
+    public void setOpcaoAcao(String opcaoAcao){
+        this.opcaoAcao = opcaoAcao;
+    }
+    public String getOpcaoAcao(){
+        return opcaoAcao;
+    }
+}
