750da452738caaa6706b5fbd86a699e9481597f4
João Ricardo <joaoricardolopes2013@gmail.com>
2025-06-03 22:41:36 -0300

    Delete Projeto - Last Frontier/src/interfaces/WeaponsActions.java

--- Projeto - Last Frontier/src/interfaces/WeaponsActions.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-import eventos.EventoCriatura;
-
-public interface WeaponsActions {
-    //Nessa interface está a assinatura do metodo usado na subclasse Armas:
-    public abstract void atacar(EventoCriatura criaturaAtacada);
-}
